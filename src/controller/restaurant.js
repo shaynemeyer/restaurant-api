e1a3430b8531543eb53ae30607c69ade4e89bbb5
Shayne Meyer <shayne.meyer@gmail.com>
2016-12-02 13:37:30 -0800

    added read and update to restaurant controller

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -5,7 +5,9 @@ import Restaurant from '../model/restaurant';
 export default ({config, db}) => {
   let api = Router();
 
-  // '/v1/restaurant/add'
+  // CRUD = Create Read Update Delete
+
+  // '/v1/restaurant/add' - Create
   api.post('/add', (req, res) => {
     let newRest = new Restaurant();
     newRest.name = req.body.name;
@@ -16,6 +18,43 @@ export default ({config, db}) => {
       }
 
       res.json({message: 'Restaurant saved successfully'});
+    });
+  });
+
+  // '/v1/restaurant' - Read
+  api.get('/', (req, res) => {
+    Restaurant.find({}, (err, restaurants) => {
+      if(err){
+        res.send(err);
+      }
+      res.json(restaurants);
+    });
+  });
+
+  // '/v1/restaurant/:id' - Read
+  api.get('/:id', (req, res) => {
+    Restaurant.findById(req.params.id, (err, restaurant) => {
+      if(err){
+        res.send(err);
+      }
+      res.json(restaurant);
+    });
+  });
+
+  // '/v1/restaurant/:id' - Read
+  api.put('/:id', (req, res) => {
+    Restaurant.findById(req.params.id, (err, restaurant) => {
+      if(err){
+        res.send(err);
+      }
+
+      restaurant.name = req.body.name;
+      restaurant.save(err=> {
+        if(err){
+          res.send(err);
+        }
+        res.json({ message: "Restaurant info updated"});
+      });
     });
   });
 
